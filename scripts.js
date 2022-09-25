dcefebad9d1be823753a4cba33f746901fa385b0
Brandon Seda <brandon@Brandons-MacBook-Air.local>
2022-09-24 22:58:24 -0400

    Updated button login and allowed for buttons to be usable to chaange colors, clear button now resets grid color

--- scripts.js
+++ scripts.js
@@ -1,35 +1,69 @@
-import style from "./Etch-A-Sketch/style.css";
+let black = '#000000';
+let white = '#FFFFFF';
+let color = black;
 
 
 const gridContainer =  document.querySelector('.grid-container'); 
+const clearBtn = document.querySelector('#clear');
+const blueBtn = document.querySelector('#blue');
+const blackBtn = document.querySelector('#black');
+const whiteBtn = document.querySelector('#white');
 
-//creates divs based on size given from user
+clearBtn.addEventListener('click', clearGrid);
+
+blueBtn.addEventListener('click', function(){
+    console.log("blue button clicked");
+    color = 'blue';
+});
+blackBtn.addEventListener('click', function(){
+    console.log("black button clicked");
+    color = black;
+
+});
+whiteBtn.addEventListener('click', function(){
+    console.log("white button clicked");
+    color = white;
+});
+
+let gridSize = 16;
+let cells = []
+
+
+//creates divs based on grid size or creates a default 16x16
 function createGrid(size){
 
-    for(let i=0; i < size; i++){
-        const gridBox = createElement('div');
-        gridBox.setAttribute('id', 'box');
-        gridBox.addEventListener('hover', changeColor)
+    let cellTotal = size * size;
+    gridContainer.style.gridTemplateColumns = ('repeat(${size}, 1fr') ;
+    gridContainer.style.gridTemplateRows = ('repeat(${size}, 1fr') ;
 
-        gridContainer.appendChild(gridBox);
+    for(let i=0; i < cellTotal; i++){
+  
+        cells[i] = document.createElement('div');
+        cells[i].classList.add('box');
+        cells[i].addEventListener('mouseover', draw);
+        cells[i].addEventListener('click', clearCell);
+        
+        gridContainer.appendChild(cells[i]);
     }
 }
+
+createGrid(5);
 
 //changes grid box color based on event happening
-function changeColor(event){
-    if (event.propertyName == 'hover'){
-       
-    }
-
-}
-
-//draws when mouse event is clicked
-function draw(){
-
+function draw(e){
+    e.target.style.background = color;
+    
 }
 
 //clears the screen on button click
-function clear(){
+function clearGrid(){
+    cells.forEach(item => {
+        item.style = 'background-color: #FFFFFF';
+    });
 
+}
+
+function clearCell(e){
+    e.target.style.background = white;
 }
 
